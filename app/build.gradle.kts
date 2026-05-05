0e7b34c2599bccea8a1ec08221a2411ee1facae4
sogonov <111385605+sogonov@users.noreply.github.com>
2026-05-05 21:38:26 +0300

    v0.1.5.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "sgnv.anubis.app"
         minSdk = 29
         targetSdk = 34
-        versionCode = 9
-        versionName = "0.1.5-beta.2"
+        versionCode = 10
+        versionName = "0.1.5.1"
     }
 
     signingConfigs {
